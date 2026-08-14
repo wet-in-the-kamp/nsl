e85ab837dbb37ac42a2f4e1500b7a2607ad169c6
wet-in-the-kamp <isaac.wetenkamp@ellisys.com>
2026-08-14 13:41:09 +0200

    mii/sgmii: Refactor to separate the backend (serdes vs transceiver) from the protocol implementation

--- lib/nsl_mii/sgmii/sgmii.pkg.vhd
+++ lib/nsl_mii/sgmii/sgmii.pkg.vhd
@@ -12,71 +12,74 @@ use work.link.all;
 package sgmii is
 
   subtype config_reg_t is std_ulogic_vector(15 downto 0);
-  
+
   type sgmii_m2p is record
-    clk_m2p_diff : nsl_io.diff.diff_pair;
-    data_m2p_diff : nsl_io.diff.diff_pair;
+    data_m2p_symbol : nsl_line_coding.ibm_8b10b.data_t;
+    align_rst       : std_ulogic;
+    sys_reset_n     : std_ulogic;
+    valid_symbol    : std_ulogic;
   end record;
 
   type sgmii_p2m is record
-    clk_p2m_diff : nsl_io.diff.diff_pair;
-    data_p2m_diff : nsl_io.diff.diff_pair;
+    data_p2m_symbol : nsl_line_coding.ibm_8b10b.data_t;
+    code_err        : std_ulogic;
+    disparity_err   : std_ulogic;
+    align_ready     : std_ulogic;
   end record;
 
   component sgmii_driver is
     generic (
-      link_timer_c : positive := 200000      -- SGMII spec says: "The link_timer
-                                             -- inside the Auto-Negotiation has been 
-                                             -- changed from 10 msec to 1.6 msec to
-                                             -- ensure a prompt update of the link status."
+      link_timer_c : positive := 200000  -- SGMII spec says: "The link_timer
+                                         -- inside the Auto-Negotiation has been 
+                                         -- changed from 10 msec to 1.6 msec to
+                                         -- ensure a prompt update of the link status."
       );
     port (
-      reset_n_i    : in  std_ulogic;
-      clock125_i   : in  std_ulogic;
-      clock625_i   : in  std_ulogic;
-      
-      sgmii_o      : out sgmii_m2p;
-      sgmii_i      : in  sgmii_p2m;
-      
+      reset_n_i : in std_ulogic;
+      clock_i   : in std_ulogic;
+
+      sgmii_o : out sgmii_m2p;
+      sgmii_i : in  sgmii_p2m;
+
       link_speed_i : in  link_speed_t := LINK_SPEED_1000;
-      link_up_o    : out std_ulogic;      
-      
-      rx_o         : out nsl_bnoc.committed.committed_req;
-      rx_i         : in  nsl_bnoc.committed.committed_ack;
-      
-      tx_i         : in  nsl_bnoc.committed.committed_req;
-      tx_o         : out nsl_bnoc.committed.committed_ack
+      link_up_o    : out std_ulogic;
+
+      rx_o : out nsl_bnoc.committed.committed_req;
+      rx_i : in  nsl_bnoc.committed.committed_ack;
+
+      tx_i : in  nsl_bnoc.committed.committed_req;
+      tx_o : out nsl_bnoc.committed.committed_ack
       );
   end component sgmii_driver;
 
   component sgmii_pcs_rx is
     port(
-      clock_i     : in  std_ulogic;
-      reset_n_i   : in  std_ulogic;
+      clock_i   : in std_ulogic;
+      reset_n_i : in std_ulogic;
 
-      symbol_i    : in  nsl_line_coding.ibm_8b10b.data_t;
-      symbol_expected_o : out std_ulogic;      
+      symbol_i          : in  nsl_line_coding.ibm_8b10b.data_t;
+      symbol_expected_o : out std_ulogic;
 
       flit_o         : out mii_flit_t;
       config_valid_o : out std_ulogic;
       config_o       : out config_reg_t;
-      valid_o        : out std_ulogic;      
+      valid_o        : out std_ulogic;
       idle_match_o   : out std_ulogic
       );
   end component;
 
   component sgmii_pcs_tx is
     port(
-      clock_i     : in  std_ulogic;
-      reset_n_i   : in  std_ulogic;
+      clock_i   : in std_ulogic;
+      reset_n_i : in std_ulogic;
 
-      flit_i      : in  mii_flit_t;
+      flit_i : in mii_flit_t;
 
-      symbol_o    : out nsl_line_coding.ibm_8b10b.data_t;
+      symbol_o : out nsl_line_coding.ibm_8b10b.data_t;
 
-      send_config_i : in  std_ulogic;
-      config_i      : in  config_reg_t;
-      link_up_i     : in  std_ulogic
+      send_config_i : in std_ulogic;
+      config_i      : in config_reg_t;
+      link_up_i     : in std_ulogic
       );
   end component;
 
@@ -85,20 +88,32 @@ package sgmii is
       link_timer_cycles_c : natural := 1250000
       );
     port(
-      clock_i     : in  std_ulogic;
-      reset_n_i   : in  std_ulogic;
+      clock_i   : in std_ulogic;
+      reset_n_i : in std_ulogic;
 
-      config_i    : in  config_reg_t;
-      restart_i   : in  std_ulogic := '0';
+      config_i  : in config_reg_t;
+      restart_i : in std_ulogic := '0';
 
-      rx_config_valid_i : in  std_ulogic;
-      rx_config_i       : in  config_reg_t;
-      rx_idle_i         : in  std_ulogic;
+      rx_config_valid_i : in std_ulogic;
+      rx_config_i       : in config_reg_t;
+      rx_idle_i         : in std_ulogic;
 
       send_config_o    : out std_ulogic;
       tx_config_o      : out config_reg_t;
       link_up_o        : out std_ulogic;
       partner_config_o : out config_reg_t
+      );
+  end component;
+
+  component sgmii_serdes_backend is
+    port (
+      reset_n_i   : in  std_ulogic;
+      clock125_i  : in  std_ulogic;
+      clock625_i  : in  std_ulogic;
+      sgmii_i     : in  sgmii_m2p;
+      sgmii_o     : out sgmii_p2m;
+      pair_diff_i : in  nsl_io.diff.diff_pair;
+      pair_diff_o : out nsl_io.diff.diff_pair
       );
   end component;
 
--- tests/inet/sgmii/src/tb.vhd
+++ tests/inet/sgmii/src/tb.vhd
@@ -17,41 +17,41 @@ use nsl_bnoc.testing.all;
 
 architecture arch of tb is
 
-  signal clock125_s, clock625_s, reset_n_s : std_ulogic;
+  signal clock125_s, clock625_s, reset_n_s      : std_ulogic;
   signal clock125_delayed_s, clock625_delayed_s : std_ulogic;
-  signal done_s : std_ulogic_vector(1 downto 0);
-  signal committed_i_s1, committed_o_s1 : nsl_bnoc.committed.committed_bus;
-  signal committed_i_s2, committed_o_s2 : nsl_bnoc.committed.committed_bus;
-  signal sgmii_m2p1, sgmii_m2p2 : nsl_mii.sgmii.sgmii_m2p;
-  signal sgmii_p2m1, sgmii_p2m2 : nsl_mii.sgmii.sgmii_p2m;
-  signal clk_diff_1 : nsl_io.diff.diff_pair;
-  signal clk_diff_2 : nsl_io.diff.diff_pair;
-  signal data_diff_1 : nsl_io.diff.diff_pair;
-  signal data_diff_2 : nsl_io.diff.diff_pair;
+  signal done_s                                 : std_ulogic_vector(1 downto 0);
+  signal committed_i_s1, committed_o_s1         : nsl_bnoc.committed.committed_bus;
+  signal committed_i_s2, committed_o_s2         : nsl_bnoc.committed.committed_bus;
+  signal sgmii_m2p1, sgmii_m2p2                 : nsl_mii.sgmii.sgmii_m2p;
+  signal sgmii_p2m1, sgmii_p2m2                 : nsl_mii.sgmii.sgmii_p2m;
+  signal data_diff_tx_1                         : nsl_io.diff.diff_pair;
+  signal data_diff_rx_1                         : nsl_io.diff.diff_pair;
+  signal data_diff_tx_2                         : nsl_io.diff.diff_pair;
+  signal data_diff_rx_2                         : nsl_io.diff.diff_pair;
 
   constant frame_tx_1_c : byte_string := from_hex(
     "02000000feed00deadbeef"
     );
 
-  constant clk_delay_c: time := 3 ns;
-  constant data_delay_c: time := 2 ns;
+  constant clk_delay_c  : time := 3 ns;
+  constant data_delay_c : time := 2 ns;
 
 begin
 
-  sgmii_gen: process
+  sgmii_gen : process
   begin
     done_s(0) <= '0';
 
     committed_put(committed_o_s1.req, committed_o_s1.ack, clock125_s,
                   frame_tx_1_c, true);
-    
+
     committed_wait(committed_o_s1.req, committed_o_s1.ack, clock125_s, 2048);
-    
+
     done_s(0) <= '1';
     wait;
   end process;
 
-  sgmii_check: process
+  sgmii_check : process
   begin
     done_s(1) <= '0';
 
@@ -60,42 +60,62 @@ begin
 
     done_s(1) <= '1';
     wait;
-  end process;  
+  end process;
 
   -- SGMII number 1
-  sgmii_driver_1: nsl_mii.sgmii.sgmii_driver
+  sgmii_driver_1 : nsl_mii.sgmii.sgmii_driver
     generic map (
-      link_timer_c => 30 -- Faster simulation than the spec default
+      link_timer_c => 30  -- Faster simulation than the spec default
       )
     port map (
       reset_n_i    => reset_n_s,
-      clock125_i      => clock125_s,
-      clock625_i      => clock625_s,      
+      clock_i      => clock125_s,
       sgmii_o      => sgmii_m2p1,
       sgmii_i      => sgmii_p2m1,
       link_speed_i => LINK_SPEED_1000,
-      rx_o => committed_i_s1.req,
-      rx_i => committed_i_s1.ack,
-      tx_i => committed_o_s1.req,
-      tx_o => committed_o_s1.ack
+      rx_o         => committed_i_s1.req,
+      rx_i         => committed_i_s1.ack,
+      tx_i         => committed_o_s1.req,
+      tx_o         => committed_o_s1.ack
+      );
+
+  sgmii_serdes_backend_1 : nsl_mii.sgmii.sgmii_serdes_backend
+    port map (
+      reset_n_i   => reset_n_s,
+      clock125_i  => clock125_s,
+      clock625_i  => clock625_s,
+      sgmii_i     => sgmii_m2p1,
+      sgmii_o     => sgmii_p2m1,
+      pair_diff_i => data_diff_rx_1,
+      pair_diff_o => data_diff_tx_1
       );
 
   -- SGMII number 2
-  sgmii_driver_2: nsl_mii.sgmii.sgmii_driver
+  sgmii_driver_2 : nsl_mii.sgmii.sgmii_driver
     generic map (
-      link_timer_c => 30 -- Faster simulation than the spec default
+      link_timer_c => 30  -- Faster simulation than the spec default
       )    
     port map (
       reset_n_i    => reset_n_s,
-      clock125_i      => clock125_delayed_s,
-      clock625_i      => clock625_delayed_s,      
+      clock_i      => clock125_delayed_s,
       sgmii_o      => sgmii_m2p2,
       sgmii_i      => sgmii_p2m2,
       link_speed_i => LINK_SPEED_1000,
-      rx_o => committed_i_s2.req,
-      rx_i => committed_i_s2.ack,
-      tx_i => committed_o_s2.req,
-      tx_o => committed_o_s2.ack
+      rx_o         => committed_i_s2.req,
+      rx_i         => committed_i_s2.ack,
+      tx_i         => committed_o_s2.req,
+      tx_o         => committed_o_s2.ack
+      );
+
+  sgmii_serdes_backend_2 : nsl_mii.sgmii.sgmii_serdes_backend
+    port map (
+      reset_n_i   => reset_n_s,
+      clock125_i  => clock125_delayed_s,
+      clock625_i  => clock625_delayed_s,
+      sgmii_i     => sgmii_m2p2,
+      sgmii_o     => sgmii_p2m2,
+      pair_diff_i => data_diff_rx_2,
+      pair_diff_o => data_diff_tx_2
       );
 
   -- Created delayed clock for SGMII driver 2
@@ -105,32 +125,24 @@ begin
   clock625_delayed_s <= transport clock625_s after clk_delay_c;
 
   -- Connect the two SGMII together with propagation delay
-  clk_diff_1 <= transport sgmii_m2p1.clk_m2p_diff after data_delay_c;
-  sgmii_p2m2.clk_p2m_diff <= clk_diff_1;
-  
-  data_diff_1 <= transport sgmii_m2p1.data_m2p_diff after data_delay_c;
-  sgmii_p2m2.data_p2m_diff <= data_diff_1;
-  
-  clk_diff_2 <= transport sgmii_m2p2.clk_m2p_diff after data_delay_c;
-  sgmii_p2m1.clk_p2m_diff <= clk_diff_2;
+  data_diff_rx_2 <= transport data_diff_tx_1 after data_delay_c;
 
-  data_diff_2 <= transport sgmii_m2p2.data_m2p_diff after data_delay_c;
-  sgmii_p2m1.data_p2m_diff <= data_diff_2;  
-  
-  driver: nsl_simulation.driver.simulation_driver
+  data_diff_rx_1 <= transport data_diff_tx_2 after data_delay_c;
+
+  driver : nsl_simulation.driver.simulation_driver
     generic map(
       clock_count => 2,
       reset_count => 1,
-      done_count => done_s'length
+      done_count  => done_s'length
       )
     port map(
-      clock_period(0) => 8 ns, -- 125MHz clock for GMII
-      clock_period(1) => 1600 ps, -- 625MHz clock for SGMII
+      clock_period(0)   => 8 ns,        -- 125MHz clock for GMII
+      clock_period(1)   => 1600 ps,     -- 625MHz clock for SGMII
       reset_duration(0) => 14 ns,
-      reset_n_o(0) => reset_n_s,
-      clock_o(0) => clock125_s,
-      clock_o(1) => clock625_s,
-      done_i => done_s
+      reset_n_o(0)      => reset_n_s,
+      clock_o(0)        => clock125_s,
+      clock_o(1)        => clock625_s,
+      done_i            => done_s
       );
 
 end;
